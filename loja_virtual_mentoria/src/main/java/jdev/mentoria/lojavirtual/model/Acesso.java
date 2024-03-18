f3cdf6a78f2b7fbc0f8ff865689861cb34213c78
Vini <visilva5690@hotmail.com>
2024-03-18 20:44:02 -0300

    Feat: Criação das tabelas e relacionamentos entre tabelas

---
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/Acesso.java
@@ -0,0 +1,70 @@
+package jdev.mentoria.lojavirtual.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private int id;
+
+	@Column(nullable = false)
+	private String descricao; /* Acesso ex: ROLE_ADMIN, ROLE_SECRETARIO */
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
